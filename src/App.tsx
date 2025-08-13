fbe25e5a6c488c8cdfce23baa2281dcddbecebcd
Rawnak Hasan <rawnakhasan86@gmail.com>
2025-08-13 22:43:58 +0600

    Responsiveness Tried

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 const App = () => {
   return (
     <div className="h-screen bg-cream flex items-center justify-center">
-      <div className="container w-2xl bg-white rounded-xl grid grid-cols-2">
-        <img
-          src="/image-product-desktop.jpg"
-          className="rounded-l-xl"
-          alt="Product"
-        />
-        <div className="p-8 flex flex-col justify-between">
+      <div className="main m-8 w-2xl bg-white rounded-xl grid sm:grid-cols-2 min-w-96 max-[643px]:h-3/4">
+        <picture>
+          <source
+            media="(min-width: 634px)"
+            srcSet="/image-product-desktop.jpg"
+          />
+          <img
+            src="/image-product-mobile.jpg"
+            className="max-[643px]:rounded-t-xl sm:rounded-l-xl"
+            alt="Product"
+          />
+        </picture>
+        <div className="p-8 flex flex-col justify-between space-y-4">
           <p className="tracking-[0.4rem] text-[0.875rem] text-grey font-medium">
             PERFUME
           </p>
